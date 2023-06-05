7fb4edbb1e58722600a2c450a416d9a3dbda80dd
Helena Saborit <helena.sb91@gmail.com>
2023-06-05 12:24:40 +0200

    creation of project,  basic components and app-routing

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes} from '@angular/router'
+import { HomeComponent } from './component/home/home.component';
+import { GamesComponent } from './component/games/games.component';
+import { ProfileComponent } from './component/profile/profile.component';
+import { ErrorPageComponent } from './component/error-page/error-page.component';
+
+const routes: Routes = [
+  {
+    path: "",
+    component: HomeComponent
+  },
+  {
+    path: "games",
+    component: GamesComponent
+  },
+  {
+    path: "profile",
+    component: ProfileComponent
+  },
+  {
+    path: "**",
+    component: ErrorPageComponent
+  }
+];
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+  RouterModule
+  ]
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,29 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './component/home/home.component';
+import { NavbarComponent } from './component/navbar/navbar.component';
+import { FooterComponent } from './component/footer/footer.component';
+import { GamesComponent } from './component/games/games.component';
+import { GameItemComponent } from './component/game-item/game-item.component';
+import { ProfileComponent } from './component/profile/profile.component';
+import { AppRoutingModule } from './app-routing.module';
+import { ErrorPageComponent } from './component/error-page/error-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    FooterComponent,
+    GamesComponent,
+    GameItemComponent,
+    ProfileComponent,
+    ErrorPageComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
