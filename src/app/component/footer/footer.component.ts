6c38290fdb7720a1631c984175f5779602da4f7b
Helena Saborit <helena.sb91@gmail.com>
2023-06-06 14:44:44 +0200

    footer done

--- src/app/component/footer/footer.component.ts
+++ src/app/component/footer/footer.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent {
-
+  photoUrl: string = '../../../assets/img/github2.png';
+  
 }
