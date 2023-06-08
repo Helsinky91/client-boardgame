f8aa35937ed3916f460de3cf32d06d958171d66e
Helena Saborit <helena.sb91@gmail.com>
2023-06-08 14:10:29 +0200

    playing with css

--- src/app/component/user/profile/profile.component.ts
+++ src/app/component/user/profile/profile.component.ts
@@ -23,7 +23,7 @@ export class ProfileComponent implements OnInit {
     if (userId) {
       this.userService.getUser(userId).subscribe((user: UserInterface) => {
          this.currentUser = user;
-         console.log("user " + currentUser.wishlist);
+        //  console.log("user " + currentUser.wishlist);
       });
     }
   }
